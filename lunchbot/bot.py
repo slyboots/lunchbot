b4bc18171a1372fc8f64783bab65454788a27565
Dakota Wisdom Lorance <dwl@dakotalorance.com>
2019-01-20 21:01:46 -0600

    added actual bot code

---
+++ lunchbot/bot.py
@@ -0,0 +1,86 @@
+'''actual bot code'''
+import os
+import re
+
+from slackclient import SlackClient
+from lunchbot import BOTNAME
+# To remember which teams have authorized your app and what tokens are
+# associated with each team, we can store this information in memory on
+# as a global object. When your bot is out of development, it's best to
+# save this in a more persistant memory store.
+authed_teams = {}
+
+def sanitize(text):
+    return re.sub(r'[^a-zA-Z0-9\s]','',text).lower()
+
+class Bot(object):
+    """ Instanciates a Bot object to handle Slack onboarding interactions."""
+    def __init__(self):
+        super(Bot, self).__init__()
+        self.name = BOTNAME
+        self.emoji = ":robot_face:"
+        # When we instantiate a new bot object, we can access the app
+        # credentials we set earlier in our local development environment.
+        self.oauth = {"client_id": os.environ.get("CLIENT_ID"),
+                      "client_secret": os.environ.get("CLIENT_SECRET"),
+                      # Scopes provide and limit permissions to what our app
+                      # can access. It's important to use the most restricted
+                      # scope that your app will need.
+                      "scope": "bot"}
+        self.verification = os.environ.get("VERIFICATION_TOKEN")
+        self.client = SlackClient("")
+        self.messages = {}
+
+    def auth(self, code):
+        """
+        Authenticate with OAuth and assign correct scopes.
+        Save a dictionary of authed team information in memory on the bot
+        object.
+        Parameters
+        ----------
+        code : str
+            temporary authorization code sent by Slack to be exchanged for an
+            OAuth token
+        """
+        auth_response = self.client.api_call(
+                                "oauth.access",
+                                client_id=self.oauth["client_id"],
+                                client_secret=self.oauth["client_secret"],
+                                code=code
+                                )
+        print(f"AUTH RESPONSE: {auth_response}")
+        team_id = auth_response["team_id"]
+        authed_teams[team_id] = {"bot_token":
+                                 auth_response["bot"]["bot_access_token"]}
+        self.client = SlackClient(authed_teams[team_id]["bot_token"])
+        print(f"AUTHED TEAMS: {authed_teams}")
+
+    def respond(self, event):
+        user = event['event']['user']
+        message = sanitize(event['event']['text'])
+        channel = event['event']['channel']
+        timestamp = event['event']['ts']
+        if self.could_make_dad_joke(message):
+            self.with_dad_joke(channel, message, timestamp)
+
+
+    def with_dad_joke(self, channel, message, timestamp):
+        name = re.sub(r'^.*(im|i am)', '', message).strip()
+        joke = f"Hi {name}, I'm {BOTNAME}"
+        self._send_message(channel, joke, thread_ts=timestamp)
+
+
+    def could_make_dad_joke(self, text):
+        return 'im hungry' in text
+
+
+    def _send_message(self,channel,text,**kwargs):
+        post_message = self.client.api_call("chat.postMessage",
+                                            channel=channel,
+                                            username=self.name,
+                                            icon_emoji=self.emoji,
+                                            as_user=True,
+                                            text=text,
+                                            **kwargs
+                                            )
+        print(f"sent message: {post_message}")
